c173b5373ee9942829e0a575e206725fab28a2af
Pablo Tirado <pablo.tirado@nilus.org>
2021-05-12 18:48:07 -0300

    delete .history

--- .history/App_20210512094854.js
+++
@@ -1,9 +0,0 @@
-import React, {useEffect, useState} from 'react';
-import SoundPlayer from 'react-native-sound-player';
-import RootNavigator from './src/navigator';
-
-const App = () => {
-  return <RootNavigator />;
-};
-
-export default App;
--- .history/App_20210512094907.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RootNavigator from './src/navigator';
-
-const App = () => {
-  return <RootNavigator />;
-};
-
-export default App;
